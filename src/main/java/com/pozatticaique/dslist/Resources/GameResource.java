1a68b35a4dcf7ea2c4e508b4453ad1c9356984c4
Caique Pozatti <caca_pozatti@hotmail.com>
2023-07-15 21:55:45 -0300

    GameMinDTO + GameService + GameResource

--- src/main/java/com/pozatticaique/dslist/Resources/GameResource.java
+++ src/main/java/com/pozatticaique/dslist/Resources/GameResource.java
@@ -1,5 +1,26 @@
 package com.pozatticaique.dslist.Resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pozatticaique.dslist.DTO.GameMinDTO;
+import com.pozatticaique.dslist.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
 public class GameResource {
+	
+	@Autowired
+	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		List<GameMinDTO> result = gameService.findAll();		
+		return result;
+	}
 
 }
--- src/main/java/com/pozatticaique/dslist/services/GameService.java
+++ src/main/java/com/pozatticaique/dslist/services/GameService.java
@@ -1,8 +1,32 @@
 package com.pozatticaique.dslist.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.pozatticaique.dslist.DTO.GameMinDTO;
+import com.pozatticaique.dslist.Repositories.GameRepository;
+import com.pozatticaique.dslist.entities.Game;
 
 @Service
 public class GameService {
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll(){
+		List<Game> result = gameRepository.findAll();
+		
+		List<GameMinDTO> dto = new ArrayList<>();
+		for(Game x : result) {
+			GameMinDTO obj = new GameMinDTO(x);
+			dto.add(obj);
+		}		
+		//List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+		return dto;
+	}
 
 }
